b6fcbbc976e667d8b603175452af0eac1ccec400
Rohit <rohit.jbcodeapp@gmail.com>
2023-08-02 18:29:25 +0530

    add font,color and make a NavBar components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import { Montserrat } from "next/font/google";
+import NavBar from "@/components/NavBar";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
